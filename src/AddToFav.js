fd8027715e87a87918207347e6c957a3d7a48e59
CharlesC51 <c.crepin@net-c.fr>
2018-10-08 21:08:00 +0200

    add button for add the game to your favorites

---
+++ src/AddToFav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "reactstrap";
+
+export default class Example extends React.Component {
+  render() {
+    return (
+      <div>
+        <Button>add this game to your favorites!</Button>
+      </div>
+    );
+  }
+}
