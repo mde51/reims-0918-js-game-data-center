9e8f2897bc8ed826b3e9ae34876a12be90bb6a86
Robin Guerre <rb.guerre@gmail.com>
2018-10-12 11:28:12 +0200

    début fonction

--- src/GameThumbnail.js
+++ src/GameThumbnail.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 
+const changeCoverSide = (cover) => {
+  const urlToArray = cover.split("/");
+  return urlToArray;
+}
+
 const GameThumbnail = ({ name, cover }) => (
   <div>
     <Card>
       <CardImg src={cover.url} alt="Card image cap" />
+      <p>() => changeCoverSide{cover.url}</p>
       <CardBody>
         <CardTitle>{name}</CardTitle>
       </CardBody>
