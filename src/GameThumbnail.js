f825b41b02cbe13ab23106e2ddf7ba1f6cfa3f82
Robin Guerre <rb.guerre@gmail.com>
2018-10-15 15:57:16 +0200

    fix crash if cover property is missing

--- src/GameThumbnail.js
+++ src/GameThumbnail.js
@@ -1,20 +1,28 @@
 import React from "react";
 import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 
-const changeCoverSide = (cover) => {
+const changeCoverSide = cover => {
   const urlToArray = cover.split("/");
-  urlToArray[6] = "t_720p"
+  urlToArray[6] = "t_720p";
   return urlToArray.join("/");
-}
+};
 
 const GameThumbnail = ({ name, cover }) => (
   <div>
-    <Card>
-      <CardImg src={changeCoverSide(cover.url)} alt="Card image cap" />
-      <CardBody>
-        <CardTitle>{name}</CardTitle>
-      </CardBody>
-    </Card>
+    {cover ? (
+      <Card>
+        <CardImg src={changeCoverSide(cover.url)} alt="Card image cap" />
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+        </CardBody>
+      </Card>
+    ) : (
+      <Card>
+        <CardBody>
+          <CardTitle>{name}</CardTitle>
+        </CardBody>
+      </Card>
+    )}
   </div>
 );
 
