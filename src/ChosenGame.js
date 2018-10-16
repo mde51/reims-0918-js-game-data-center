dec76fbe60e4524d483f5675b2141fe92de2573d
Robin Guerre <rb.guerre@gmail.com>
2018-10-16 17:24:37 +0200

    chosenGame simplifié, console.log retirés

--- src/ChosenGame.js
+++ src/ChosenGame.js
@@ -1,11 +1,9 @@
 import React from "react";
-import GameThumbnail from "./GameThumbnail";
+import ChosenGameThumbnail from "./ChosenGameThumbnail";
 
 const ChosenGame = ({ game }) => (
   <div>
-    <GameThumbnail {...game} />
-    <p>Résumé jeu sélectionné : {game.summary}</p>
-    <p>Storyline : {game.storyline}</p>
+    <ChosenGameThumbnail {...game} />
   </div>
 );
 
