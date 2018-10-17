2a7bad7df77a8108ec980076e33b1b921b12ebf7
Robin Guerre <rb.guerre@gmail.com>
2018-10-17 10:07:10 +0200

    DisplayChosenComponent merged with ChosenGame

--- src/ChosenGame.js
+++ src/ChosenGame.js
@@ -1,9 +1,24 @@
 import React from "react";
-import DisplayChosenGame from "./DisplayChosenGame";
+import { Card, CardImg, CardBody, CardTitle, CardText } from "reactstrap";
+
+const changeCoverSize = cover => {
+  const urlToArray = cover.split("/");
+  urlToArray[6] = "t_1080p";
+  return urlToArray.join("/");
+};
 
 const ChosenGame = ({ game }) => (
   <div>
-    <DisplayChosenGame {...game} />
+    <Card>
+      {game.cover && (
+        <CardImg src={changeCoverSize(game.cover.url)} alt="Card image cap" />
+      )}
+      <CardBody>
+        <CardTitle>{game.name}</CardTitle>
+        <CardText>{game.summary}</CardText>
+        <CardText>{game.storyline}</CardText>
+      </CardBody>
+    </Card>
   </div>
 );
 
