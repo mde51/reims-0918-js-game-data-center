e299564fae046cf5e6d5adb5205257cee79e1daa
CharlesC51 <c.crepin@net-c.fr>
2018-10-17 16:29:43 +0200

    delete FORM and delete button Search

--- src/ResearchBar.js
+++ src/ResearchBar.js
@@ -1,23 +1,18 @@
 import React from "react";
-import { Button, Form, FormGroup, Input } from "reactstrap";
+import { Input } from "reactstrap";
 
-const ResearchBar = ({value, onChange}) => {
+const ResearchBar = ({ value, onChange }) => {
   return (
-    <Form>
-      <FormGroup>
-        <Input
-          type="search"
-          name="nameSearch"
-          id="idSearch"
-          placeholder="Write your game here !"
-          value={value}
-          onChange={onChange}
-        />
-      </FormGroup>
-      <Button class="btn btn-outline-success" type="submit">
-        Search
-      </Button>
-    </Form>
+    <div>
+      <Input
+        type="search"
+        name="nameSearch"
+        id="idSearch"
+        placeholder="Write your game here !"
+        value={value}
+        onChange={onChange}
+      />
+    </div>
   );
 };
 
