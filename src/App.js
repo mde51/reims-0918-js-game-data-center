16945a7498dd8e15f44483e643ea488103ca5c6f
Gautier <gautier08000@hotmail.com>
2018-10-19 17:14:19 +0200

    fix add player name button

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ class App extends Component {
   submitNewPlayer() {
     this.setState({
       players: [...this.state.players, {
-        name: this.state.tempPlayers,
+        name: this.state.tempPlayer,
       }]
     });
   }
