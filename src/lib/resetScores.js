80603e51643a784d473d412659b9d32bf163be15
Robin Guerre <robin.guerre@yahoo.fr>
2018-10-25 17:41:05 +0200

    wip resetScores

---
+++ src/lib/resetScores.js
@@ -0,0 +1,16 @@
+// export const resetScores = players => players.map(player => console.log(player.name))
+
+
+// it("should reset finalScore & inputScore to 0", () => {
+//     const players = [
+//       { name: "Michel", finalScore: 3, inputScore: 3 },
+//       { name: "Charles", finalScore: 6, inputScore: 6 },
+//       { name: "Gautier", finalScore: 4, inputScore: 4 }
+//     ];
+//     const expected = [
+//       { name: "Michel", finalScore: 0, inputScore: 0 },
+//       { name: "Charles", finalScore: 0, inputScore: 0 },
+//       { name: "Gautier", finalScore: 0, inputScore: 0 }
+//     ];
+//     expect(resetScores(players)).toEqual(expected);
+//   });
