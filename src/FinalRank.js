cc2e31f30a9cd079b1203419ea108e08e52400c0
Robin Guerre <robin.guerre@yahoo.fr>
2018-10-26 10:17:12 +0200

    final scores displayed

---
+++ src/FinalRank.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FinalRank = ({ name, i, score}) => {
+  return (
+    <tr>
+      <th>{i=i+1}</th>
+      <td>{name}</td>
+      <td>{score}</td>
+    </tr>
+  );
+};
+
+export default FinalRank;
