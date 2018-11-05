4054742cf78b1920deab391de78cbdbebf0aceac
Simonin Gautier <gautier08000@hotmail.com>
2018-11-05 14:53:07 +0100

    fix pr

--- src/Footer.js
+++ src/Footer.js
@@ -1,21 +1,23 @@
 import React from "react";
 
-const Footer = () => {
+const Footer = ({}) => {
   return (
     <div>
-      <ul>
-        <li className="footergame">
-          <a class="nav-link text-white" href="#">
-            ©Game Date Center
-          </a>
-        </li>
-        <li className="footermentions">
-          <a class="nav-link text-white" href="#">
-            Mentions Légales
-          </a>
-        </li>
-        <li className="footername">Made By: Charles, Robin, Michel, Gautier</li>
-      </ul>
+        <ul>
+          <li className="footergame">
+            <a className="nav-link text-white" href="#">
+              ©Game Date Center
+            </a>
+          </li>
+          <li className="footermentions">
+            <a className="nav-link text-white" href="#">
+              Mentions Légales
+            </a>
+          </li>
+          <li className="footername">
+            Made By: Charles, Robin, Michel, Gautier
+          </li>
+        </ul>
     </div>
   );
 };
