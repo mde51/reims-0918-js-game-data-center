4c30df6423d1ffffb4f8bde9b2824525e78b64fb
Robin Guerre <robin.guerre@yahoo.fr>
2018-11-06 00:38:16 +0100

    erase commentary from newRound.test

--- src/lib/newRound.test.js
+++ src/lib/newRound.test.js
@@ -100,32 +100,3 @@ it("button New Round", () => {
     expectedHistory
   );
 });
-
-// const currentRound = [
-//   { name: "Michel", finalScore: 3, inputScore: 3 },
-//   { name: "Charles", finalScore: 6, inputScore: 6 },
-//   { name: "Gautier", finalScore: 4, inputScore: 4 }
-// ];
-// const currentHistory = {
-//   gameId: [
-//     [
-//       { name: "Michel", score: 2 },
-//       { name: "Charles", score: 5 },
-//       { name: "Gautier", score: 1 }
-//     ]
-//   ]
-// };
-// const expectedHistory = {
-//   gameId: [
-//     [
-//       { name: "Michel", score: 2 },
-//       { name: "Charles", score: 5 },
-//       { name: "Gautier", score: 1 }
-//     ],
-//     [
-//       { name: "Michel", score: 3 },
-//       { name: "Charles", score: 6 },
-//       { name: "Gautier", score: 4 }
-//     ]
-//   ]
-// };
