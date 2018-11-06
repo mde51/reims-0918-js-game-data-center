2cf4b91fe5a62605c9770fa922f9477ec69ac70d
Michel <descotesmichel@aol.com>
2018-11-06 17:44:17 +0100

    mise en forme tableau round

--- src/OneRound.js
+++ src/OneRound.js
@@ -2,21 +2,23 @@ import React from "react";
 import { Table } from "reactstrap";
 
 const OneRound = ({ round }) => (
-  <Table dark>
-    <thead>
+  <Table>
+  <table className="table table-striped">
+    <thead className="tableauRound">
       <tr>
         <th>name</th>
         <th>score</th>
       </tr>
     </thead>
     {round.map((player, indexPlayer) => (
-      <tbody key={indexPlayer}>
+      <tbody key={indexPlayer} className="table-striped" >
         <tr>
           <td>{player.name}</td>
           <td>{player.score}</td>
         </tr>
       </tbody>
     ))}
+    </table>
   </Table>
 );
 
