210766d44a8207614c876cf01bb1c41227dd4971
CharlesC51 <c.crepin@net-c.fr>
2018-11-06 11:31:57 +0100

    fix conflict

--- src/GameThumbnail.js
+++ src/GameThumbnail.js
@@ -1,22 +1,5 @@
-<<<<<<< HEAD
-import React from 'react'
-import { Card, CardImg, CardBody,
-    CardTitle } from 'reactstrap';
-  
-  const GameThumbnail = () => (
-      <div>
-        <Card>
-          <CardImg src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-          <CardBody>
-            <CardTitle>Game title</CardTitle>
-          </CardBody>
-        </Card>
-      </div>
-  );
-=======
 import React from "react";
 import { Card, CardImg, CardBody, CardTitle, } from "reactstrap";
->>>>>>> 4407859f642e0ed01cbe6dc2fd95a99d7b4d4a25
 
 const changeCoverSize = cover => {
   const urlToArray = cover.split("/");
--- src/MainJumbotron.js
+++ src/MainJumbotron.js
@@ -5,13 +5,9 @@ const MainJumbotron = ({ handleDisplayFavs }) => {
   return (
     <Jumbotron className="mt-1">
       <Container>
-<<<<<<< HEAD
-        <Row><h1 className="display-3">GAME DATA CENTER</h1></Row>
-=======
         <section>
           <img src={require("./Logo.png")} alt="" />
         </section>
->>>>>>> 4407859f642e0ed01cbe6dc2fd95a99d7b4d4a25
         <Row>
           <Col>
             <Button color="primary">Recently played</Button>
